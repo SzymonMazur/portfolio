f86bb54039e1761c0fbd699cdf6fdc653929e61c
wagnam <szma001@student.kristiania.no>
2022-05-11 20:48:33 +0200

    d

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,7 +10,7 @@
                 <h4 class="fs-5" style="font-weight: 400;">Email:</h4>
                 <a class="fs-4 nav-link p-0" href="mailto:szymon_98@icloud.com?Subject=Get%20In%20touch" target="_top" >szymon_98@icloud.com</a>
                 <h4 class="mt-5 fs-5" style="font-weight: 400;">By:
-                    <span>
+                    <span class="pb-5">
                         <a class="fs-4 mt-3 nav-link p-0" href="https://www.linkedin.com/in/szymon-mazur-07554b15b/">LinkedIn</a>
                     </span>
                     
